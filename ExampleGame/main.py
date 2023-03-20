0b38880f78b02a28f61e32dcafb5aa8420a6bf67
Louis Gambardella <merwynyt@gmail.com>
2023-03-20 10:47:44 +0100

    update

--- ExampleGame/main.py
+++ ExampleGame/main.py
@@ -1,6 +1,7 @@
-import os, sys
-sys.path.insert(0, os.path.abspath("."))
+if __name__ == "__main__":
+    import os, sys
+    sys.path.insert(0, os.path.abspath("."))
 
-from Shyne import Shyne
+    from Shyne import Shyne
 
-shyne = Shyne("ExampleGame")
+    shyne = Shyne("ExampleGame")
